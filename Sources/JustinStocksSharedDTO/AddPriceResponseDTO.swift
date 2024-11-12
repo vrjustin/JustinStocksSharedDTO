cd2ceb83fa1ad76dfe1b0c5dabbb93d9b40cae3a
Justin Maronde <myemailisjustin@me.com>
2024-11-12 12:59:38 -0500

    AddPriceResponseDTO library object

---
+++ Sources/JustinStocksSharedDTO/AddPriceResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  AddPriceResponseDTO.swift
+//  JustinStocksSharedDTO
+//
+//  Created by Justin Maronde on 11/12/24.
+//
+
+import Foundation
+
+public struct AddPriceResponseDTO: Codable {
+    public let id: UUID
+    public let symbol: String
+    public let date: Date
+    public let open: Double
+    public let close: Double
+    
+    public init(id: UUID, symbol: String, date: Date, open: Double, close: Double) {
+        self.id = id
+        self.symbol = symbol
+        self.date = date
+        self.open = open
+        self.close = close
+    }
+}
